2033712e607bdc9aa17d0040a1592fbe98fc12da
alexey F <52556121+fedenko03@users.noreply.github.com>
2023-02-15 16:35:52 +0600

    I'm working on an algorithm for returning keys

--- keyreturner/models.py
+++ keyreturner/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from user.models import MainUser
+
+
+class SettingsKeyReturner(models.Model):
+    token = models.CharField(max_length=100)
+    token_timestamp = models.DateTimeField(blank=True, null=True)
+    in_process = models.BooleanField(default=False)
+    user = models.ForeignKey(MainUser, on_delete=models.CASCADE, blank=True, null=True)
+    step = models.IntegerField(default=1)
+    error = models.CharField(max_length=100, default='', blank=True, null=True)
+
+    def __str__(self):
+        return self.token
+
+    class Meta:
+        verbose_name = 'Settings KeyReturner'
+        verbose_name_plural = 'Settings KeyReturner'
--- keyreturner/views.py
+++ keyreturner/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def keyreturnerMain(request):
+    return render(request, 'keyreturner-step1.html')
