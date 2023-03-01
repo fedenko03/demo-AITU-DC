5185061c5758a220f0da6ba954f5c814942ac448
alexey F <52556121+fedenko03@users.noreply.github.com>
2023-03-01 16:37:59 +0600

    v

--- AITUDC/azure.py
+++ AITUDC/azure.py
@@ -6,25 +6,4 @@ ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.env
 CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    # Enables whitenoise for serving static files
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'main.middleware.PinCodeMiddleware',
-    'main.middleware.NotFoundMiddleware'
-]
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-SECRET_KEY = os.getenv('SECRET_KEY')
-
 DEBUG = False
