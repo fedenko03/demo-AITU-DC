81c9eb9c5d819b31ebb164ac732ad164f38b5410
alexey F <52556121+fedenko03@users.noreply.github.com>
2023-03-01 23:56:53 +0600

    v1

--- static/js/js-notifications.js
+++ static/js/js-notifications.js
@@ -1,6 +1,6 @@
 // websocket
 
-var socket = new WebSocket("ws://" + window.location.host + "/ws/new_order/");
+var socket = new WebSocket("wss://" + window.location.host + "/ws/new_order/");
 socket.onmessage = function (e) {
     var data = JSON.parse(e.data);
     createNotification(data)
