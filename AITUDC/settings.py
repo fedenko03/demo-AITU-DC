b22e401027ca6a639841a14990f0b59b00fbb58a
alexey F <52556121+fedenko03@users.noreply.github.com>
2023-03-01 22:18:56 +0600

    v

--- AITUDC/settings.py
+++ AITUDC/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-g%-q+0fz@39_uu0u=ta8#(%erxr%=+690b_ev&x!%2o@8lr$2q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['testaitu.azurewebsites.net', '*']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -167,3 +167,8 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'alexfedenko0303200306@gmail.com'
 EMAIL_HOST_PASSWORD = 'vbtfilqdusgxseqn'
+
+
+import os
+if 'WEBSITE_HOSTNAME' in os.environ: # Running on Azure
+    from .azure import *
