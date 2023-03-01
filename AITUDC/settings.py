fb1b99d3ff8ed537acbd89fecfb15c1d780de7bd
alexey F <52556121+fedenko03@users.noreply.github.com>
2023-03-02 00:07:09 +0600

    v1

--- AITUDC/settings.py
+++ AITUDC/settings.py
@@ -148,8 +148,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- AITUDC/urls.py
+++ AITUDC/urls.py
@@ -30,5 +30,5 @@ urlpatterns = [
     # path('login/', LoginView.as_view(), name='login'),
 ]
 
-
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
