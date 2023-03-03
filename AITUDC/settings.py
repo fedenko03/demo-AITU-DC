2276ba2698ed24485e7690d6f69c7b34e402782b
alexey F <52556121+fedenko03@users.noreply.github.com>
2023-03-03 22:29:03 +0600

    workkkkkkkkkkk

--- AITUDC/settings.py
+++ AITUDC/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -145,6 +145,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -171,6 +172,6 @@ EMAIL_HOST_USER = 'alexfedenko0303200306@gmail.com'
 EMAIL_HOST_PASSWORD = 'vbtfilqdusgxseqn'
 
 
-import os
-if 'WEBSITE_HOSTNAME' in os.environ: # Running on Azure
-    from .azure import *
+# import os
+# if 'WEBSITE_HOSTNAME' in os.environ: # Running on Azure
+#     from .azure import *
